c1ed5323c6f2dc96e4e26e8397c0aa78650e90ba
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-06-30 11:56:38 -0400

    Peninsula 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Peninsula Shopping Center",
                     "default_image" : "http://assets.codecloudapp.com/sites/574c69996e6f641030000000/image/jpeg/undefined/psc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "1589822182"
+                    "social_feed" : "http://peninsula.mallmaverick.com/api/v2/peninsula/social.json"
                 }
